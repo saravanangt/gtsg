9c2c556bb029a170b307cbdf2b670105d519ebb2
saravanangt <42960405+saravanangt@users.noreply.github.com>
2019-03-02 16:28:59 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+    "text": "Robert DeSoto added a new task",
+    "attachments": [
+        {
+            "fallback": "Plan a vacation",
+            "author_name": "Owner: rdesoto",
+            "title": "Plan a vacation",
+            "text": "I've been working too hard, it's time for a break.",
+            "actions": [
+                {
+                    "name": "action",
+                    "type": "button",
+                    "text": "Complete this task",
+                    "style": "",
+                    "value": "complete"
+                },
+                {
+                    "name": "tags_list",
+                    "type": "select",
+                    "text": "Add a tag...",
+                    "data_source": "static",
+                    "options": [
+                        {
+                            "text": "Launch Blocking",
+                            "value": "launch-blocking"
+                        },
+                        {
+                            "text": "Enhancement",
+                            "value": "enhancement"
+                        },
+                        {
+                            "text": "Bug",
+                            "value": "bug"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+};
+   console.log(response);
+   res.send(response);
+})
+
+var server = app.listen(8383, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
